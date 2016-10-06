6ad3b30cec768e14fec085f665cc481a48f5492e
aurenen <aurenen@users.noreply.github.com>
2016-10-05 22:20:38 -0700

    Working visitor tracker

--- includes/functions.php
+++ includes/functions.php
@@ -83,11 +83,16 @@ function getCount($id) {
 
     // select hit count for this site
     $stmt = $db->prepare("SELECT `count` FROM `" . DB_PREFIX . "projects` WHERE `site_id` = :id;");
-    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-    $stmt->execute();
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    try {
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    return $result['count'];
+        return $result['count'];
+    }
+    catch (Exception $ex) {
+        echo $ex->getMessage();
+    }
 }
 
 function addCount($id) {
@@ -97,12 +102,142 @@ function addCount($id) {
 
     // update hit count with $count
     $stmt = $db->prepare("UPDATE `" . DB_PREFIX . "projects` SET `count` = :count WHERE `site_id` = :id;");
-    $stmt->bindParam(':count', $count, PDO::PARAM_INT);
-    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-    $stmt->execute();
+    try {
+        $stmt->bindParam(':count', $count, PDO::PARAM_INT);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $db = null;
+        return $count;
+    }
+    catch (Exception $ex) {
+        echo $ex->getMessage();
+    }
+}
+
+// http://php.net/manual/en/function.get-browser.php#101125
+function getBrowser() { 
+    $u_agent = $_SERVER['HTTP_USER_AGENT']; 
+    $bname = 'Unknown';
+    $platform = 'Unknown';
+    $version= "";
+
+    //First get the platform?
+    if (preg_match('/linux/i', $u_agent)) {
+        $platform = 'linux';
+    }
+    elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
+        $platform = 'mac';
+    }
+    elseif (preg_match('/windows|win32/i', $u_agent)) {
+        $platform = 'windows';
+    }
+
+    // Next get the name of the useragent yes seperately and for good reason
+    if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent)) 
+    { 
+        $bname = 'Internet Explorer'; 
+        $ub = "MSIE"; 
+    } 
+    elseif(preg_match('/Firefox/i',$u_agent)) 
+    { 
+        $bname = 'Mozilla Firefox'; 
+        $ub = "Firefox"; 
+    } 
+    elseif(preg_match('/Chrome/i',$u_agent)) 
+    { 
+        $bname = 'Google Chrome'; 
+        $ub = "Chrome"; 
+    } 
+    elseif(preg_match('/Safari/i',$u_agent)) 
+    { 
+        $bname = 'Apple Safari'; 
+        $ub = "Safari"; 
+    } 
+    elseif(preg_match('/Opera/i',$u_agent)) 
+    { 
+        $bname = 'Opera'; 
+        $ub = "Opera"; 
+    } 
+    elseif(preg_match('/Netscape/i',$u_agent)) 
+    { 
+        $bname = 'Netscape'; 
+        $ub = "Netscape"; 
+    } 
+
+    // finally get the correct version number
+    $known = array('Version', $ub, 'other');
+    $pattern = '#(?<browser>' . join('|', $known) .
+    ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
+    if (!preg_match_all($pattern, $u_agent, $matches)) {
+        // we have no matching number just continue
+    }
+
+    // see how many we have
+    $i = count($matches['browser']);
+    if ($i != 1) {
+        //we will have two since we are not using 'other' argument yet
+        //see if version is before or after the name
+        if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
+            $version= $matches['version'][0];
+        }
+        else {
+            $version= $matches['version'][1];
+        }
+    }
+    else {
+        $version= $matches['version'][0];
+    }
+
+    // check if we have a number
+    if ($version==null || $version=="") {$version="?";}
+
+    return array(
+        'userAgent' => $u_agent,
+        'name'      => $bname,
+        'version'   => $version,
+        'platform'  => $platform,
+        'pattern'    => $pattern
+    );
+}
+
+// http://stackoverflow.com/a/13646735
+function getUserIp()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+
+    if (filter_var($client, FILTER_VALIDATE_IP))
+        $ip = $client;
+    else if (filter_var($forward, FILTER_VALIDATE_IP))
+        $ip = $forward;
+    else
+        $ip = $remote;
+
+    return $ip;
+}
+
+function addHitInfo($id, $referrer, $page_hit, $ip_address, $browser_info, $time) {
+    $db = db_connect();
+
+    $query = "INSERT INTO `" . DB_PREFIX . "hits` (`site_id`, `referrer`, `page`, `ip_address`, `browser`, `time`)
+        VALUES (:site_id, :referrer, :page, :ip_address, :browser, :time);";
+    $stmt = $db->prepare($query);
+    try {
+        $stmt->bindParam(':site_id', $id, PDO::PARAM_INT);
+        $stmt->bindParam(':referrer', $referrer, PDO::PARAM_STR, 200);
+        $stmt->bindParam(':page', $page_hit, PDO::PARAM_STR, 50);
+        $stmt->bindParam(':ip_address', $ip_address, PDO::PARAM_STR, 20);
+        $stmt->bindParam(':browser', $browser_info, PDO::PARAM_STR, 30);
+        $stmt->bindParam(':time', $time, PDO::PARAM_STR);
+        $stmt->execute();
+    }
+    catch (Exception $ex) {
+        echo $ex->getMessage();
+    }
 
     $db = null;
-    return $count;
 }
 
 function getSettings() {
--- track.php
+++ track.php
@@ -23,6 +23,25 @@ if ($site_id == 0)
 addCount($site_id);
 
 // grab visitor info and inserts a new row with it, associated with the site id
+if (strlen($_GET['ref']) < 1)
+    $link_ref = "None";
+else 
+    $link_ref = $_GET['ref'];
+
+if (strlen($_GET['page']) < 1)
+    $link_hit = "None";
+else
+    $link_hit = $_GET['page'];
+$user_ip = getUserIp();
+$browser = getBrowser();
+$hit_time = date('Y-m-d H:i:s');
+/*
+    [name] => Google Chrome
+    [version] => 53.0.2785.143
+    [platform] => mac
+ */
+$browser_format = sprintf('%s %.1f (%s)', $browser['name'], $browser['version'], $browser['platform']);
+addHitInfo($site_id, $link_ref, $link_hit, $user_ip, $browser_format, $hit_time);
 
 // print image
 $image = @imagecreate(1, 1) or die ("Error creating image.");
